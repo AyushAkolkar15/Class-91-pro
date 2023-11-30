70fef76bfc9bbb1a1a5e3ad09519d18133abccba
AyushAkolkar15 <121815916+AyushAkolkar15@users.noreply.github.com>
2023-11-30 21:38:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import HomeScreen from "./Screen/Home"
+
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+
+export default function App() {
+  return (
+  <HomeScreen></HomeScreen>
+  );
+}
+
